4b3e9588bbc2989a6e4932a793adaa70f006dcf3
Vishal580 <R20200530.VISHAL@cse.reva.edu.in>
2024-12-12 17:51:08 +0530

    Fetching Notes from Context

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import noteContext from '../context/notes/noteContext';
 
 function Home() {
+  const context = useContext(noteContext);
+  const {notes, setNotes} = context;
   return (
-    <div>
-      This is Home
+    <div className='container my-3'>
+        <h1>Add a Note</h1>
+          <form>
+              <div className="mb-3">
+                  <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                  <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                      <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+              </div>
+              <div className="mb-3">
+                  <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                  {/* <input type="password" className="form-control" id="exampleInputPassword1"/> */}
+              </div>
+              <button type="submit" className="btn btn-primary">Save</button>
+          </form>
+          <div className='container my-3'>
+            <h1>Your Notes</h1>
+            {notes.map((note)=>{
+                return note.title;
+            })}
+          </div>
     </div>
   )
 }
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,8 +1,40 @@
+import { useState } from "react";
 import NoteContext from "./noteContext";
 
 const NoteState = (props) => {
+    const notesInitial = [
+        {
+          "_id": "675944b397a698e5050256b6",
+          "user": "675824644e3088c90f820107",
+          "title": "Updated title 2",
+          "description": "Updated Description 2",
+          "tag": "Updated Tag",
+          "date": "2024-12-11T07:52:19.153Z",
+          "__v": 0
+        },
+        {
+          "_id": "675944b397a698e5050256b8",
+          "user": "675824644e3088c90f820107",
+          "title": "My Title3",
+          "description": "My description",
+          "tag": "My Tag",
+          "date": "2024-12-11T07:52:19.998Z",
+          "__v": 0
+        },
+        {
+          "_id": "675944b497a698e5050256ba",
+          "user": "675824644e3088c90f820107",
+          "title": "My Title3",
+          "description": "My description",
+          "tag": "My Tag",
+          "date": "2024-12-11T07:52:20.631Z",
+          "__v": 0
+        }
+      ]
+    const [notes, setNotes] = useState(notesInitial);
+
     return(
-     <NoteContext.Provider value={{}}>
+     <NoteContext.Provider value={{notes, setNotes}}>
         {props.children}
      </NoteContext.Provider>
     )
